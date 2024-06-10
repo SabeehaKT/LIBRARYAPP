c286672b5e1fb36e6ac39a4bb6542de1670950b6
SabeehaKT <kampalasabeeha@gmail.com>
2024-06-10 23:05:44 +0530

    library-app updated

--- src/Components/AddBook.jsx
+++ src/Components/AddBook.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
 
 const AddBook = () => {
+    const [data,setData]=useState(
+        {
+            "title" :"",
+            "category":"",
+            "pubyr":"",
+            "author":"",
+            "dist":"",
+            "pubname":"",
+            "price":"",
+            "edition":""
+        }
+    )
+    const inputHandler = (event)=>{
+        setData({...data,[event.target.name]:event.target.value})
+    }
+
+    const readValue=()=>{
+        console.log(data)
+    }
   return (
     <div>
         <NavBar/>
@@ -11,11 +30,11 @@ const AddBook = () => {
                     <div className="row g-3">
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Book Title</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name="title" value={data.title} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Category</label>
-                            <select name="" id="" className="form-control">
+                            <select name="category" id="" className="form-control" value={data.category} onChange={inputHandler}>
                                 <option value="Science">Science</option>
                                 <option value="Fiction">Fiction</option>
                                 <option value="Classic">Classic</option>
@@ -24,30 +43,30 @@ const AddBook = () => {
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Published Year</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name="pubyr" value={data.pubyr} onChange={inputHandler} />
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                             <label htmlFor="" className="form-label">AuthorName</label>
-                            <textarea name="" id="" className="form-control"></textarea>
+                            <textarea name="author" id="" className="form-control" value={data.author} onChange={inputHandler}></textarea>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Distributer Name</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name="dist" value={data.dist} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Publisher Name</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control"  name="pubname" value={data.pubname} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Price</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control"  name="price" value={data.price} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Edition</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name="edition" value={data.edition} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <button className="btn btn-success">Add</button>
+                            <button className="btn btn-success" onClick={readValue}>Add</button>
                         </div>
                     </div>
                 </div>
--- src/Components/ViewTable.jsx
+++ src/Components/ViewTable.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
 
 const ViewTable = () => {
+    const [data, setData] = useState([
+        {
+            "title": "Station Eleven",
+            "author": "Emily St. John Mande",
+            "pubyr": "2014",
+            "price": "Rs.3500"
+        },
+        {
+            "title": "The Catcher in The Rye",
+            "author": "Thomas Edison",
+            "pubyr": "2002",
+            "price": "Rs.1200"
+        }
+
+    ])
     return (
         <div>
-            <NavBar/>
+            <NavBar />
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -21,43 +36,16 @@ const ViewTable = () => {
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <th scope="row">Harry Potter</th>
-                                            <td>J K Rowling</td>
-                                            <td>1998</td>
-                                            <td>Rs.5965</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">The Catcher in The Rye</th>
-                                            <td>Thomas Edison</td>
-                                            <td>2002</td>
-                                            <td>Rs.1200</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">Educator</th>
-                                            <td>Tara Watsover</td>
-                                            <td>2012</td>
-                                            <td>Rs.2500</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">Normal People</th>
-                                            <td>Sally Rooney</td>
-                                            <td>2018</td>
-                                            <td>Rs.1685</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">Gone Girl</th>
-                                            <td>Andy Wier</td>
-                                            <td>2023</td>
-                                            <td>Rs.2185</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">Station Eleven</th>
-                                            <td>Emily St. John Mande</td>
-                                            <td>2014</td>
-                                            <td>Rs.3500</td>
-                                        </tr>
-
+                                        {data.map(
+                                            (value, index) => {
+                                                return <tr>
+                                                    <td scope="row">{value.title}</td>
+                                                    <td>{value.author}</td>
+                                                    <td>{value.pubyr}</td>
+                                                    <td>{value.price}</td>
+                                                </tr>
+                                            }
+                                        )}
                                     </tbody>
                                 </table>
                             </div>
